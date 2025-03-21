fe9358ab8fee3abcfc9d6aa28f2de6704344fa20
yauhen_bialetski <yauhen_bialetski@epam.com>
2025-03-21 18:39:00 +0100

    Lesson13

--- lesson_13/firstTest.test.js
+++ lesson_13/firstTest.test.js
@@ -1,9 +1,17 @@
+const path = require('path');
+
 describe('Our first test', () => {
   test('check boolean', async () => {
+    const filePath = `..\\..\\data\\Company.xml.zip`
+    const pathTaFile = path.join(__dirname, filePath);
+    console.log(pathTaFile)
     await expect(true).toEqual(true)
   })
 
   test('check boolean', async () => {
+    const filePath = `../../data/Company.xml.zip`
+    const pathTaFile = path.join(__dirname, filePath);
+    console.log(pathTaFile)
     await expect(false).toEqual(false)
   })
 });
