d3ecbe7d3fb72cd4c0c69026e3bb4b9881b4bf64
yauhen_bialetski <yauhen_bialetski@epam.com>
2025-03-21 19:02:43 +0100

    Lesson13

--- lesson_13/firstTest.test.js
+++ lesson_13/firstTest.test.js
@@ -2,15 +2,19 @@ const path = require('path');
 
 describe('Our first test', () => {
   test('check boolean', async () => {
-    const filePath = `..\\..\\data\\Company.xml.zip`
-    const pathTaFile = path.join(__dirname, filePath);
-    console.log(pathTaFile)
+    const filePath = `..\\..\\data\\Company.xml.zip`;
+    const fileName = filePath.split('\\')[filePath.split('\\').length - 1];
+    const pathTaFile = path.join(process.cwd(), 'test', 'data', fileName);
+    const pathTaFile1 = path.resolve(fileName);
+    console.log('1 - ', pathTaFile)
+    console.log('2 - ', pathTaFile1)
+    console.log('cwd - ', process.cwd());
     await expect(true).toEqual(true)
   })
 
   test('check boolean', async () => {
     const filePath = `../../data/Company.xml.zip`
-    const pathTaFile = path.join(__dirname, filePath);
+    const pathTaFile = path.join(process.cwd(), filePath);
     console.log(pathTaFile)
     await expect(false).toEqual(false)
   })
